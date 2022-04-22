5723aa54d16ea1d7c0532040d67b693df0fc58f1
Vikarious <fialaire.arnaud@free.fr>
2022-04-22 12:31:09 +0200

    asdf

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Category;
-use App\Form\CategoryType;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,23 +17,4 @@ class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/category/add', name: 'category_add')]
-    public function addCategory(Request $request): Response
-    {
-        $category = new Category();
-
-        $form = $this->createForm(CategoryType::class, $category);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($category);
-            $em->flush();
-            return $this->redirectToRoute('admin_home');
-        }
-        
-        return $this->render('admin/category/add.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
 }
